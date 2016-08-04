1c0929870ac2aba35546bfaabdfabffe1a032cdb
timteo <timothy.teo@titansoft.com.sg>
2016-08-04 12:28:07 +0800

    Extract superclass CommandWriter to handle write() and getSize()

---
+++ GeneralizationCs/CommandWriter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace GeneralizationCs
+{
+    public abstract class CommandWriter
+    {
+        public List<string> CommandParameterList { get; }
+        private readonly char[] commandChar;
+
+        public CommandWriter(char[] commandChar)
+        {
+            CommandParameterList = new List<string>();
+            this.commandChar = commandChar;
+        }
+
+        protected void AddCommandParamters(params string[] parameters)
+        {
+            foreach (var parameter in parameters)
+            {
+                CommandParameterList.Add(parameter);
+            }
+        }
+
+        protected abstract void AddCommandParamters();
+
+        private int GetMessageSize()
+        {
+            int totalSize = GetFixedHeadersSize();
+            foreach (var parameter in CommandParameterList)
+            {
+                totalSize += parameter.Length + 1;
+            }
+            return totalSize;
+        }
+
+        private int GetFixedHeadersSize()
+        {
+            return Constant.Header.Length + Constant.SIZE_LENGTH + Constant.CMD_BYTE_LENGTH + Constant.Footer.Length;
+        }
+
+        public void Write(TextWriter writer)
+        {
+            AddCommandParamters();
+
+            writer.Write(Constant.Header);
+            writer.Write(GetMessageSize());
+            writer.Write(commandChar);
+
+            foreach (var parameters in CommandParameterList)
+            {
+                writer.Write(parameters);
+                writer.Write(Constant.SEPERATOR);
+            }
+
+            writer.Write(Constant.Footer);
+        }
+    }
+}
--- GeneralizationCs/LoginCommand.cs
+++ GeneralizationCs/LoginCommand.cs
@@ -1,37 +1,21 @@
-using System;
-using System.IO;
-
 namespace GeneralizationCs
 {
-	public class LoginCommand
-	{
-		private string name;
-		private string password;
-        
-		private static char[] commandChar = {(char)0x01};
+    public class LoginCommand : CommandWriter
+    {
+        private string name;
+        private string password;
 
 
+        public LoginCommand(string name, string password)
+            : base(CommandCharacterMapping.CommandMappings[typeof (LoginCommand)])
+        {
+            this.name = name;
+            this.password = password;
+        }
 
-		public LoginCommand(string name, string password)
-		{
-			this.name = name;
-			this.password = password;
-		}
-
-		private int getSize() {
-			return Constant.Header.Length + Constant.SIZE_LENGTH + Constant.CMD_BYTE_LENGTH + Constant.Footer.Length +
-				name.Length + 1 +
-				password.Length + 1;
-		}
-		public void Write(TextWriter writer) {
-			writer.Write(Constant.Header);
-			writer.Write(getSize());
-			writer.Write(commandChar);
-			writer.Write(name);
-			writer.Write(Constant.SEPERATOR);
-			writer.Write(password);
-			writer.Write(Constant.SEPERATOR);
-			writer.Write(Constant.Footer);
-		}
-	}
+        protected override void AddCommandParamters()
+        {
+            AddCommandParamters(name, password);
+        }
+    }
 }
