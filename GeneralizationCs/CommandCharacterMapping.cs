91cc798974c43ee8c222464926e6b9836dbd874d
timteo <timothy.teo@titansoft.com.sg>
2016-08-04 12:08:30 +0800

    Extract commandChar into CommandCharacterMapping

---
+++ GeneralizationCs/CommandCharacterMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneralizationCs
+{
+    public class CommandCharacterMapping
+    {
+        public static Dictionary<Type, char[]> CommandMappings { get; private set; }
+
+        static CommandCharacterMapping()
+        {
+            CommandMappings = new Dictionary<Type, char[]>();
+            CommandMappings.Add(typeof(LoginCommand), new char[] { (char)0x01 });
+            CommandMappings.Add(typeof(AddEmployeeCmd), new char[] { (char)0x02 });
+        }
+    }
+}
