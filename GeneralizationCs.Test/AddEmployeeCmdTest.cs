8f721d2c05630f9194d63c5435c76a17deb36553
timteo <timothy.teo@titansoft.com.sg>
2016-08-05 10:55:59 +0800

    Wrong namespace name

--- GeneralizationCs.Test/AddEmployeeCmdTest.cs
+++ GeneralizationCs.Test/AddEmployeeCmdTest.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 
-namespace GeneralizationCs
+namespace GeneralizationCs.Test
 {
     [TestFixture]
     public class AddEmployeeCmdTest
